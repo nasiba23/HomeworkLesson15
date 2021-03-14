607b04e52a74b3fd0342cf93fe155bfea96221ba
Nasiba <nasyrova.nasiba@gmail.com>
2021-03-14 20:33:18 +0500

    implemented CRUD operations

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
 
 namespace HomeworkLesson15
 {
@@ -6,7 +9,46 @@ namespace HomeworkLesson15
     {
         static void Main(string[] args)
         {
-            
+            //Create
+            using (DatabaseContext context = new DatabaseContext())
+            {
+                var person = new Person();
+                person.BirthDate = DateTime.Now;
+                person.FirstName = "Test";
+                person.LastName = "Test";
+                person.MiddleName = "Test";
+                context.People.Add(person);
+                context.SaveChanges();
+            }
+            //Read
+            using (var context = new DatabaseContext())
+            {
+                var people = context.People.ToList();
+                foreach (var person in people)
+                {
+                    Console.WriteLine($"Name - {person.FirstName}\t Surname - {person.LastName}");
+                }
+            }
+            //Update
+            using (var context = new DatabaseContext())
+            {
+                var person = context.People.FirstOrDefault(p => p.Id == 3);
+                if (person != null)
+                {
+                    person.FirstName = "updated";
+                    context.SaveChanges();
+                }
+            }
+            //Delete
+            using (var context = new DatabaseContext())
+            {
+                var person = context.People.FirstOrDefault(p => p.Id == 4);
+                if (person != null)
+                {
+                    context.People.Remove(person);
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
